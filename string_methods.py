fcfba1620c4fe14eed5aff80dbdbaa9d7b551420
rohanravula <109545469+rohanravula@users.noreply.github.com>
2022-07-30 22:37:45 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,39 @@
+# a="rohan"
+# print(a.upper()) #ROHAN. when we type upper the output will be in all upper case.this is by storing in variable.
+# print("rohan".upper()) #ROHAN. the upper case method can be used without storing in any variables also. this is method 2
+
+# print("ROHAN".lower()) #rohan. when we type lower the output will be in lower case.
+# print("PYTHON".casefold()) #python. the casefold is also acts as like a lower case.
+
+# print("computer".isupper()) #False. when we use the isupper if the word is in upper case it shows "True" if not it shows "False" .
+# print("COMPUTER".isupper()) #True.
+
+# print("MobILe".islower()) #False. Wehn we use the islower if the word is in lower case it shows "True" if not it shows "False."
+# print("mobile".islower()) #True. the word mobile is in lower case it shows "True."
+
+# print("KumAr".swapcase()) #kUMaR. swapcase is used for the changing the upper  to lower and lower to upper case.
+
+# print("rOHan KumaR".capitalize()) #Rohan kumar. this method is used for the to make starting letter into uppper case and remaning letters into lower case.
+# print("raVULa RoHAn kUMAR".capitalize()) #Ravula rohan kumar.
+
+# print("raVULa rohAN kUMAr".title()) #Ravula Rohan Kumar. the starting words become upper case and rest all will be in lower case. after the space the word will also become upper case.
+# print("One Plus".istitle()) #True. this method tells that ifnthe starting word is upper case then it shows true if not there it shows False.
+# print("one plus".istitle()) #False. coz it is not in uppercase so the output shows False.
+# print("One plus".istitle()) #False
+# print("one Plus".istitle()) #False
+
+# print("python code".startswith("fyt")) #False. coz the starting letter starts with p i,e. pyt so its showing False.
+# print("python code".startswith('pyt')) #True. it shows True coz the stating letter starts with pyt so its shows True.
+# print("rohan".startswith("Roh")) #False. it shows False coz the starting word is lower case and in bracket we insert in upper case. 
+# print("rohan".startswith('r')) #True
+# print("rohan".startswith('o')) #False
+
+# print("ravulas".endswith('las')) #True.it shows True coz the ending letter end with las so its shows True.
+# print("ravulas".endswith('sal')) #False.it shows False coz the ending letters end with las not with sal so it shows False.
+
+# print(len("pythonprograme")) #14. this length (len) about the how many letters are their in that word. and if we give space that also will be counted.
+# print(len("python programe")) #15. we gave space so the space is also count so its showing as 15.
+
+# print("python".find("t")) #2. it tell the at what position the word is lying. and the counting is stated from zero "0".
+# print("ravula rohan".find("h")) #9. in this the space is also counted.
+print("python".find("r"))
